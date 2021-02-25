b147de938e28c290089b603ac47926c939da1687
Hadiiz <hadiziadeh@hotmail.com>
2021-02-25 20:59:54 +0200

    Bug Fixes

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const book_arr = [];
+const add_book = document.getElementById("add-button");
+const form_containter = document.getElementById("form-section");
+const form = document.getElementById("form-section");
+const submit = document.getElementById("submit-btn")
+
+add_book.addEventListener('click', e=>{form.style.display="block"});
+form_containter.addEventListener('click', e =>{
+    if(e.target.id == "form-section")
+        form.style.display ="none";
+});
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
-    this.pages = pages;4
+    this.pages = pages;
     this.read = read;
-    this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, 
-                ${this.read == false ? "not read yet": "read"}`;
-    }
+    // this.info = function(){
+    //     return `${this.title} by ${this.author}, ${this.pages} pages, 
+    //             ${this.read == false ? "not read yet": "read"}`;
+    // }
 }
